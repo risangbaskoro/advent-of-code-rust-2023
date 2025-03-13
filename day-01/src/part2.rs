2e847508686a91fada9cdfdab0c8159ebcbc8d97
Risang Baskoro <contact@risangbaskoro.com>
2025-03-13 17:30:25 +0700

    Day 01 Part 2

--- day-01/src/part2.rs
+++ day-01/src/part2.rs
@@ -1,6 +1,56 @@
+use std::iter::from_fn;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    let result =
+        input.lines().map(process_line).sum::<u32>();
+
+    Ok(result.to_string())
+}
+
+fn process_line(line: &str) -> u32 {
+    let mut index = 0;
+
+    let line_iter = from_fn(move || {
+        let reduced = &line[index..];
+
+        let result = if reduced.starts_with("one") {
+            Some('1')
+        } else if reduced.starts_with("two") {
+            Some('2')
+        } else if reduced.starts_with("three") {
+            Some('3')
+        } else if reduced.starts_with("four") {
+            Some('4')
+        } else if reduced.starts_with("five") {
+            Some('5')
+        } else if reduced.starts_with("six") {
+            Some('6')
+        } else if reduced.starts_with("seven") {
+            Some('7')
+        } else if reduced.starts_with("eight") {
+            Some('8')
+        } else if reduced.starts_with("nine") {
+            Some('9')
+        } else {
+            let result = reduced.chars().next();
+            result
+        };
+        index += 1;
+        result
+    });
+
+    let mut it =
+        line_iter.filter_map(|c: char| c.to_digit(10));
+
+    let first = it.next().expect("Should be a number");
+
+    match it.last() {
+        Some(num) => format!("{first}{num}"),
+        None => format!("{first}{first}"),
+    }
+    .parse::<u32>()
+    .expect("Should be a valid number")
 }
 
 #[cfg(test)]
@@ -9,9 +59,14 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!("281", process(input)?);
         Ok(())
     }
 }
