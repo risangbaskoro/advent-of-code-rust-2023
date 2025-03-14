113c1000f9da1d0d685106dcd7976d1ddeefe747
Risang Baskoro <contact@risangbaskoro.com>
2025-03-14 08:36:21 +0700

    Day 02 Part 1

---
+++ day-02/src/part1.rs
@@ -0,0 +1,107 @@
+use std::collections::BTreeMap;
+
+use miette::miette;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{
+        self, alpha1, line_ending, multispace1,
+    },
+    multi::separated_list1,
+    sequence::{preceded, separated_pair},
+    IResult,
+};
+
+#[derive(Debug)]
+struct Cube<'a> {
+    color: &'a str,
+    amount: u32,
+}
+
+#[derive(Debug)]
+struct Game<'a> {
+    id: u32,
+    rounds: Vec<Vec<Cube<'a>>>,
+}
+
+impl Game<'_> {
+    fn valid_for_cube_set(
+        &self,
+        map: &BTreeMap<&str, u32>,
+    ) -> Option<u32> {
+        self.rounds
+            .iter()
+            .all(|round| {
+                round.iter().all(|current_cube| {
+                    current_cube.amount
+                        <= *map
+                            .get(current_cube.color)
+                            .unwrap()
+                })
+            })
+            .then_some(self.id)
+    }
+}
+
+fn parse_games(input: &str) -> IResult<&str, Vec<Game>> {
+    separated_list1(line_ending, parse_game)(input)
+}
+
+fn parse_game(input: &str) -> IResult<&str, Game> {
+    let (input, id) =
+        preceded(tag("Game "), complete::u32)(input)?;
+
+    let (input, rounds) = preceded(
+        tag(": "),
+        separated_list1(tag("; "), parse_round),
+    )(input)?;
+
+    Ok((input, Game { id, rounds }))
+}
+
+fn parse_round(input: &str) -> IResult<&str, Vec<Cube>> {
+    separated_list1(tag(", "), parse_cube)(input)
+}
+
+fn parse_cube(input: &str) -> IResult<&str, Cube> {
+    let (input, (amount, color)) =
+        separated_pair(complete::u32, multispace1, alpha1)(
+            input,
+        )?;
+
+    Ok((input, Cube { amount, color }))
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String> {
+    let map = BTreeMap::from([
+        ("red", 12),
+        ("green", 13),
+        ("blue", 14),
+    ]);
+
+    let (_, games) = parse_games(input)
+        .map_err(|e| miette!("Error parsing {e}"))?;
+
+    let result: u32 = games
+        .iter()
+        .filter_map(|game| game.valid_for_cube_set(&map))
+        .sum();
+
+    Ok(result.to_string())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        assert_eq!("8", process(input)?);
+        Ok(())
+    }
+}
